62f3dfe00efe31e21e0e752c0ffebf2ba8fa49ab
Aditya Wanwade <Adiwanwade@gmail.com>
2023-12-26 12:07:07 +0530

    Made changes into files for fixing database error

--- resources/views/components/primary-button.blade.php
+++ resources/views/components/primary-button.blade.php
@@ -1,3 +1,5 @@
+
+
 <button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center px-4 py-2 rounded-md font-semibold text-white uppercase tracking-widest hover:bg-orange-600 focus:bg-orange-600 active:bg-orange-600 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150', 'style' => 'background-color: #ff9900;']) }}>
     {{ $slot }}
 </button>
